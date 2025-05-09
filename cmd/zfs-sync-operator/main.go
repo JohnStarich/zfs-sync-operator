960649686cee125fa33942e2f3429b294ee08ea8
John Starich <johnstarich@johnstarich.com>
2025-05-08 23:35:42 -0500

    Release leader on shutdown for quick tests

--- cmd/zfs-sync-operator/main.go
+++ cmd/zfs-sync-operator/main.go
@@ -6,9 +6,11 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log/slog"
 	"os"
 	"os/signal"
 
+	"github.com/go-logr/logr"
 	"github.com/johnstarich/zfs-sync-operator/internal/zfsbackup"
 	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
@@ -39,16 +41,22 @@ func run(ctx context.Context, args []string, out io.Writer) error {
 		return errors.New("zfs-sync-operator: this command does not take any arguments")
 	}
 
+	logger := logr.FromSlogHandler(slog.NewJSONHandler(out, nil))
+
 	fmt.Fprintln(out, "Ready!")
 	restConfig, err := config.GetConfig()
 	if err != nil {
 		return err
 	}
 	mgr, err := manager.New(restConfig, manager.Options{
-		LeaderElection:          true,
-		LeaderElectionID:        "zfs-sync-operator.johnstarich.com",
-		LeaderElectionNamespace: "zfs-sync-operator-system",
-		BaseContext:             func() context.Context { return ctx },
+		LeaderElection:                true,
+		LeaderElectionID:              "zfs-sync-operator.johnstarich.com",
+		LeaderElectionNamespace:       "zfs-sync-operator-system",
+		LeaderElectionReleaseOnCancel: true,
+		Logger:                        logger,
+		BaseContext: func() context.Context {
+			return ctx
+		},
 	})
 	if err != nil {
 		return err
@@ -71,4 +79,8 @@ func run(ctx context.Context, args []string, out io.Writer) error {
 	}
 
 	return mgr.Start(ctx)
+}
+
+func toPointer[Value any](value Value) *Value {
+	return &value
 }
