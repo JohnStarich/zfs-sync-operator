b7daf0eedf7a248cc37e83f52d69832b446dfebe
John Starich <johnstarich@johnstarich.com>
2025-05-08 23:20:42 -0500

    Add basic controller watching pods

--- cmd/zfs-sync-operator/main.go
+++ cmd/zfs-sync-operator/main.go
@@ -8,6 +8,15 @@ import (
 	"io"
 	"os"
 	"os/signal"
+
+	"github.com/johnstarich/zfs-sync-operator/internal/zfsbackup"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
 func main() {
@@ -20,7 +29,7 @@ func main() {
 	}
 }
 
-func run(_ context.Context, args []string, out io.Writer) error {
+func run(ctx context.Context, args []string, out io.Writer) error {
 	flagSet := flag.NewFlagSet("", flag.ContinueOnError)
 	err := flagSet.Parse(args)
 	if err != nil {
@@ -31,5 +40,35 @@ func run(_ context.Context, args []string, out io.Writer) error {
 	}
 
 	fmt.Fprintln(out, "Ready!")
-	return nil
+	restConfig, err := config.GetConfig()
+	if err != nil {
+		return err
+	}
+	mgr, err := manager.New(restConfig, manager.Options{
+		LeaderElection:          true,
+		LeaderElectionID:        "zfs-sync-operator.johnstarich.com",
+		LeaderElectionNamespace: "zfs-sync-operator-system",
+		BaseContext:             func() context.Context { return ctx },
+	})
+	if err != nil {
+		return err
+	}
+
+	ctrl, err := controller.New("zfsbackup", mgr, controller.Options{
+		Reconciler: zfsbackup.New(mgr.GetClient()),
+	})
+	if err != nil {
+		return err
+	}
+
+	if err := ctrl.Watch(source.Kind(mgr.GetCache(), &corev1.Pod{}, &handler.TypedEnqueueRequestForObject[*corev1.Pod]{})); err != nil {
+		return err
+	}
+
+	// TODO validate ZFSBackup
+	if err := builder.WebhookManagedBy(mgr).For(&corev1.Pod{}).Complete(); err != nil {
+		return err
+	}
+
+	return mgr.Start(ctx)
 }
